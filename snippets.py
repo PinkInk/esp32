27265a4904e52082a44359b16b1c225942668408
Tim Pelling <tim.pelling@gmail.com>
2017-06-26 19:16:36 +0400

    snippet 2d rotating polyhedron

--- snippets.py
+++ snippets.py
@@ -42,3 +42,36 @@ for c in s:
     x0 += font.w-1
 d.show()
 # --------------------------------------------------------
+
+# --------------------------------------------------------
+import machine, math
+from ssd1306 import SSD1306
+
+d = SSD1306(machine.I2C(scl=machine.Pin(4), sda=machine.Pin(5)))
+
+def rotate(x, y, cx, cy, t, flatten=False):
+    if flatten:
+        return int(math.cos(t) * (x - cx) - math.sin(t) * (y - cy) + cx),\
+                int(math.sin(t) * (x - cx) + math.cos(t) * (y - cy) + cy)
+    else:
+        return math.cos(t) * (x - cx) - math.sin(t) * (y - cy) + cx,\
+                math.sin(t) * (x - cx) + math.cos(t) * (y - cy) + cy
+
+cx, cy = d.width//2-1, d.height//2-1
+yd = 0
+i = 0
+di = 1
+while True:
+    points = []
+    for a in range(0, 360, 60):
+        points.append(rotate(cx, cy-i, cx, cy, math.radians(a+yd), True))
+    d.fill(0)
+    for p1 in points:
+        for p2 in points:
+            d.line(p1[0], p1[1], p2[0], p2[1], 1)
+    d.show()
+    yd += 5
+    i += di
+    if i == 64 or i == 0:
+        di = -di
+# --------------------------------------------------------
