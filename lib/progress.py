8d11a4184ea7aca4d392e842b1b6b62ab4be97b8
Tim Pelling <tim.pelling@gmail.com>
2017-07-03 20:14:28 +0400

    progress bar

---
+++ lib/progress.py
@@ -0,0 +1,22 @@
+def progress(
+        display,
+        x, y, width,
+        v_min=0, v_max=100,
+        initial_value=0,
+        suffix='%', prefix='',
+        color=1
+    ):
+
+    from d2 import scale_linear
+    y_scale = scale_linear(v_min, v_max, x+1, x+width-2)
+
+    def update(value, frame=False):
+        if frame:
+            display.rect(x, y, width, 10, color)
+        display.fill_rect(x+1, y+1, y_scale(max), 8, 0)
+        display.text(prefix+str(value)+suffix, x+1, y+1, color)
+        display.invert_rect(x+1, y+1, y_scale(value), 8)
+
+    update(initial_value, True)
+
+    return update
