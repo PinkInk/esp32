06fe3f37b0fa6d29e1375260544f212593f12926
Tim Pelling <tim.pelling@gmail.com>
2017-07-03 20:23:36 +0400

    fix

--- lib/progress.py
+++ lib/progress.py
@@ -13,7 +13,7 @@ def progress(
     def update(value, frame=False):
         if frame:
             display.rect(x, y, width, 10, color)
-        display.fill_rect(x+1, y+1, y_scale(max), 8, 0)
+        display.fill_rect(x+1, y+1, y_scale(v_max), 8, 0)
         display.text(prefix+str(value)+suffix, x+1, y+1, color)
         display.invert_rect(x+1, y+1, y_scale(value), 8)
 
