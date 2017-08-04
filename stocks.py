edfe0a6071168e08f9b9175eebfec50d58f85c96
Tim Pelling <tim.pelling@gmail.com>
2017-08-04 15:59:33 +0400

    pretty output

--- stocks.py
+++ stocks.py
@@ -1,5 +1,6 @@
 import d2
 import framebuf
+import freesans20
 import gc
 import machine
 import math
@@ -7,16 +8,20 @@ import ssd1306
 import time
 import ujson
 import urequests
+from writer import Writer
 
 gc.collect()
 
-exchange, stock, currency = 'LON', 'CLLN', 'GBP'
-step = 10
+exchange, stock = 'LON', 'CLLN'
+step = 10 # round axes to nearest
 
 i2c = machine.I2C(scl=machine.Pin(4), sda=machine.Pin(5))
 d = ssd1306.SSD1306(i2c)
 buf = bytearray(d.width*(d.height//2)//8)
 g = framebuf.FrameBuffer(buf, d.width, d.height//2, framebuf.MVLSB)
+fs20=Writer(d, freesans20, verbose=False)
+
+gc.collect()
 
 def get_current(exchange, stock):
     url = 'http://finance.google.com/finance/info?client=ig&q={}:{}'.format(exchange, stock)
@@ -31,8 +36,9 @@ def get_current(exchange, stock):
             d = ujson.loads(r.content[4:])[0]
         else:
             d = None
-        r.close() # must cleanup
-        return d
+
+    r.close() # must cleanup
+    return d
 
 
 ticks = [] # max 128
@@ -40,18 +46,15 @@ ticks = [] # max 128
 while True:
 
     start = time.ticks_ms()
-
-    tick = get_current('LON', 'CLLN')
-
+    tick = get_current(exchange, stock)
 
     if tick:
 
         ticks.append(round(float(tick['l']), 2))
-
-        d.fill(0)
-
         while len(ticks) > d.width:
             del ticks[0] # delete oldest ticks
+        
+        d.fill(0)
 
         gmax = math.ceil(max(ticks)/step)*step
         gmin = math.floor(min(ticks)/step)*step
@@ -80,17 +83,29 @@ while True:
 
         d.blit(g, 0, d.height//2, 0)
 
-        delta = ''
+        diff, delta = 0, ''
         try:
-            diff = ticks[-1]-ticks[-2]
-            dsign = '+' if diff > 0 else ''
-            delta = dsign + str(round(diff, 2))
+            diff = ticks[-1] - ticks[-2]
+            delta = ('+' if diff > 0 else '') + str(round(diff, 2))
         except:
             pass
-
-        d.text('{} {} {}'.format(str(ticks[-1]), currency, delta), 0, 0, 1)
-        d.text('{}:{}'.format(exchange, stock), 0, 8, 1)
-        d.text('{}'.format(tick['ltt']), 0, 16, 1)
+        
+        # direction arrow
+        if diff:
+            d.line(3, 6, 3, 18, 1 )
+            if diff > 0:
+                d.line(0, 9, 3, 6, 1)
+                d.line(3, 6, 5, 9, 1)
+            else: # diff < 0
+                d.line(0, 15, 3, 18, 1)
+                d.line(3, 18, 5, 15, 1)
+        
+        fs20.set_textpos(4, 8) # quirk: row,col i.e. reversal of x,y
+        fs20.printstring('{}'.format(str(ticks[-1])))
+        d.text(stock, d.width-len(stock)*8, 0, 1)
+        ltt = tick['ltt'].split(' ')[0]
+        d.text('{}'.format(ltt), d.width-len(ltt)*8, 8, 1)
+        d.text('{}'.format(delta), d.width-len(delta)*8, 16, 1)
 
         d.show()
 
